c4fde095baed102777e1c894999f42c409ae2a80
Bruno <cocacola_bruno@hotmail.com>
2019-11-26 17:06:43 -0300

    Corrigido build.gradle que não está correto para versão do Mixin, Mixin alterado para a versão '0.7.11'.

--- src/main/java/io/github/crucible/grimoire/Grimoire.java
+++ src/main/java/io/github/crucible/grimoire/Grimoire.java
@@ -1,19 +1,31 @@
 package io.github.crucible.grimoire;
 
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
+import org.spongepowered.asm.launch.MixinBootstrap;
+import org.spongepowered.asm.lib.tree.ClassNode;
+import org.spongepowered.asm.mixin.MixinEnvironment;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+import org.spongepowered.asm.service.MixinService;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.logging.Logger;
 
 @IFMLLoadingPlugin.Name("Grimoire")
 @IFMLLoadingPlugin.MCVersion("1.7.10")
-public class Grimoire implements IFMLLoadingPlugin {
+public class Grimoire implements IFMLLoadingPlugin, IMixinConfigPlugin {
 
     private static Logger logger = Logger.getLogger("Grimoire");
     private static Grimoire instance;
 
     public Grimoire(){
+        System.setProperty("mixin.debug.export", "true");
+        System.setProperty("mixin.env.disableRefMap","true");
+        MixinBootstrap.init();
         instance = this;
+        System.out.println("Teste");
 
     }
 
@@ -42,11 +54,46 @@ public class Grimoire implements IFMLLoadingPlugin {
 
     @Override
     public void injectData(Map<String, Object> data) {
-
     }
 
     @Override
     public String getAccessTransformerClass() {
         return null;
     }
+
+    @Override
+    public void onLoad(String mixinPackage) {
+
+    }
+
+    @Override
+    public String getRefMapperConfig() {
+        return null;
+    }
+
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+        return false;
+    }
+
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+
+    }
+
+    @Override
+    public List<String> getMixins() {
+        return null;
+    }
+
+    @Override
+    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+    }
+
+    @Override
+    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+    }
+
 }
