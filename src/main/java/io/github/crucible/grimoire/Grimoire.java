99671fe4515b357ff77a03401a70ad6e40a3b547
juanmuscaria <juanmuscaria@gmail.com>
2019-11-27 13:58:22 -0300

    Removido o submódulo e limpado varias coisas.

--- src/main/java/io/github/crucible/grimoire/Grimoire.java
+++ src/main/java/io/github/crucible/grimoire/Grimoire.java
@@ -1,39 +1,81 @@
 package io.github.crucible.grimoire;
 
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
-import io.github.crucible.grimoire.core.MixinModLoader;
+import net.minecraft.launchwrapper.LaunchClassLoader;
 import org.spongepowered.asm.launch.MixinBootstrap;
-import org.spongepowered.asm.lib.tree.ClassNode;
-import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
-import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+import org.spongepowered.asm.mixin.Mixins;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.logging.Logger;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 @IFMLLoadingPlugin.Name("Grimoire")
 @IFMLLoadingPlugin.MCVersion("1.7.10")
-public class Grimoire implements IFMLLoadingPlugin, IMixinConfigPlugin {
+public class Grimoire implements IFMLLoadingPlugin {
 
-    private static Logger logger = Logger.getLogger("Grimoire");
-    private static Grimoire instance;
-
-    public Grimoire(){
-        System.setProperty("mixin.debug.export", "true");
-
+    public Grimoire() {
         MixinBootstrap.init();
-        instance = this;
+        if (!(Grimoire.class.getClassLoader() instanceof LaunchClassLoader))
+            throw new RuntimeException("Coremod plugin class was loaded by another classloader!");
 
-        MixinModLoader.init();
+        LaunchClassLoader classLoader = (LaunchClassLoader) Grimoire.class.getClassLoader();
+
+        File dir = new File("grimoire");
+        if (!dir.exists()) {
+            System.out.println("[Grimoire] Creating mixins folder.");
+            if (!dir.mkdirs()) System.err.println("[Grimoire] Unable to create the folder " + dir.getAbsolutePath());
+        }
+
+        File[] files;
+        List<File> loadedFiles = new ArrayList<>();
+        if ((files = dir.listFiles()) != null) {
+            for (File mod : files) {
+                if (mod.isDirectory() || !mod.canRead() || !mod.getName().endsWith(".jar")) continue;
+
+                try {
+                    ZipFile zipFile = new ZipFile(mod);
+                    Enumeration<? extends ZipEntry> entries = zipFile.entries();
+                    List<ZipEntry> mixinEntries = new ArrayList<>();
+
+                    while (entries.hasMoreElements()) {
+                        ZipEntry entry = entries.nextElement();
+                        if (isMixinConfiguration(entry.getName())) {
+                            mixinEntries.add(entry);
+                        }
+                    }
+
+                    if (!mixinEntries.isEmpty()) {
+                        classLoader.addURL(mod.toPath().toUri().toURL());
+                        loadedFiles.add(mod);
+                        mixinEntries.forEach(json -> Mixins.addConfiguration(json.getName()));
+                    }
+
+                } catch (Exception e) {
+                    System.err.println("[Grimoire] Unable to load \'" + mod.getAbsolutePath() + "\'.");
+                    e.printStackTrace();
+                }
+            }
+
+            StringBuilder fileList = new StringBuilder();
+            for (File file : loadedFiles) {
+                if (fileList.length() > 0)
+                    fileList.append(", ");
+                fileList.append(file.getName());
+            }
+            System.out.println(String.format("[Grimoire] Loaded files %s","(" + loadedFiles.size() + "): " + fileList.toString()));
+
+        }
     }
 
-    public static Grimoire getInstance() {
-        return instance;
-    }
-
-    public static Logger getLogger() {
-        return logger;
+    private static boolean isMixinConfiguration(String name) {
+        String splitName = name.substring(name.lastIndexOf("/")); // Caso o zip esteja usando o "\" esse zip está quebrado, já que o padrão é o "/".
+        if (splitName.startsWith("mixin.") && splitName.endsWith(".json")) return true;
+        if (splitName.startsWith("mixin-") && splitName.endsWith(".json")) return true;
+        return splitName.endsWith("-mixin.json");
     }
 
     @Override
@@ -59,39 +101,4 @@ public class Grimoire implements IFMLLoadingPlugin, IMixinConfigPlugin {
     public String getAccessTransformerClass() {
         return null;
     }
-
-    @Override
-    public void onLoad(String mixinPackage) {
-
-    }
-
-    @Override
-    public String getRefMapperConfig() {
-        return null;
-    }
-
-    @Override
-    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
-        return false;
-    }
-
-    @Override
-    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
-
-    }
-
-    @Override
-    public List<String> getMixins() {
-        return null;
-    }
-
-    @Override
-    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
-    }
-
-    @Override
-    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
-
-    }
-
 }
