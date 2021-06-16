d8dddd50f0f58b6300cd6d05fa7e9d5aa89270c4
Extegral <VictorShadow-Integral@yandex.ru>
2021-06-16 14:26:35 +0200

    Add commons-codec to shared project

--- Grimoire-shared/src/main/java/io/github/crucible/omniconfig/annotation/lib/ClassSet.java
+++ Grimoire-shared/src/main/java/io/github/crucible/omniconfig/annotation/lib/ClassSet.java
@@ -4,19 +4,20 @@ import com.google.common.base.Preconditions;
 
 import io.github.crucible.omniconfig.OmniconfigCore;
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 public final class ClassSet<T> implements Iterable<Class<? extends T>> {
     private final Set<Class<? extends T>> classes = new HashSet<>();
     private final Class<T> baseClass;
 
-    public ClassSet(@Nonnull Class<T> baseClass) {
+    public ClassSet(@NotNull Class<T> baseClass) {
         this.baseClass = baseClass;
         Preconditions.checkArgument(baseClass != Class.class, "baseClass must not be java.lang.Class");
     }
@@ -29,7 +30,7 @@ public final class ClassSet<T> implements Iterable<Class<? extends T>> {
         return instance != null && this.contains((Class<? extends T>) instance.getClass());
     }
 
-    public boolean contains(@Nonnull Class<? extends T> clazz) {
+    public boolean contains(@NotNull Class<? extends T> clazz) {
         return this.contains(clazz, true);
     }
 
@@ -37,7 +38,7 @@ public final class ClassSet<T> implements Iterable<Class<? extends T>> {
         return instance != null && this.contains((Class<? extends T>) instance.getClass(), checkHierarchy);
     }
 
-    public boolean contains(@Nonnull Class<? extends T> clazz, boolean checkHierarchy) {
+    public boolean contains(@NotNull Class<? extends T> clazz, boolean checkHierarchy) {
         if (this.baseClass.isAssignableFrom(clazz)) {
             if (this.classes.contains(clazz))
                 return true;
@@ -62,15 +63,15 @@ public final class ClassSet<T> implements Iterable<Class<? extends T>> {
         this.classes.clear();
     }
 
-    public boolean add(@Nonnull Class<? extends T> clazz) {
+    public boolean add(@NotNull Class<? extends T> clazz) {
         return this.baseClass.isAssignableFrom(clazz) && this.classes.add(clazz);
     }
 
-    public void addRaw(@Nonnull Collection<String> classNames) {
+    public void addRaw(@NotNull Collection<String> classNames) {
         classNames.forEach(this::addRaw);
     }
 
-    public void addRaw(@Nonnull String className) {
+    public void addRaw(@NotNull String className) {
         try {
             Class<?> clazz = Class.forName(className);
             if (this.baseClass.isAssignableFrom(clazz)) {
